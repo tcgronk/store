08ac16cb86a1130033c4b338d8cc7ad206d96f32
Tess Gronski <tessgronski@gmail.com>
2020-09-05 18:50:22 -0700

    Adds cart icon to side menu

--- src/Side-Menu/MenuOptions.js
+++ src/Side-Menu/MenuOptions.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./MenuOptions.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 class MenuOptions extends React.Component {
   render() {
     let lists = this.props.list;
+    console.log(lists)
     return (
       <div className="MenuOptions">
         <ul>
           {lists.map((list) => (
-            <li key={list}>
-              <Link to={list}>{list}</Link>
+            <li key={list.name}>
+              <Link to={list.name}><FontAwesomeIcon icon={list.icon}></FontAwesomeIcon> {list.name}</Link>
             </li>
           ))}
         </ul>
--- src/Side-Menu/SideMenu.js
+++ src/Side-Menu/SideMenu.js
@@ -3,11 +3,17 @@ import logo from "./My Post-4.jpg"
 import MenuOptions from "./MenuOptions"
 import { Link } from "react-router-dom";
 import "./SideMenu.css"
-
+import {
+  faShoppingCart
+} from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+library.add(
+  faShoppingCart
+)
 class SideMenu extends React.Component{
 render(){
-    let list1=["Cart", "Log in", "Create Account"]
-    let list2=["New Arrivals", "Female Founder", "Boss Lady","Trend Setter","Lounge"]
+    let list1=[{icon: faShoppingCart, name: 'Cart'}, {name: "Log in"}, {name: "Create Account"}]
+    let list2=[{name: "New Arrivals"}, {name: "Female Founder"},{name:  "Boss Lady"},{name: "Trend Setter"},{name: "Lounge"}]
   return (
     <div className="SideMenu">
         <Link to='/'><img className='Logo' src={logo} alt='company-logo'/></Link>
